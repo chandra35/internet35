0ae42907f8323e258be337eca6caaa9483463d48
chandra35 <chandra35ok@gmail.com>
2026-04-18 00:25:37 +0700

    test: target ZTE OLT

--- test_scan_uncfg.php
+++ test_scan_uncfg.php
@@ -3,8 +3,11 @@ require __DIR__ . '/vendor/autoload.php';
 $app = require __DIR__ . '/bootstrap/app.php';
 $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-$olt = App\Models\Olt::first();
-echo 'OLT: ' . $olt->name . ' (' . $olt->ip_address . ')' . PHP_EOL;
+$olt = App\Models\Olt::where('ip_address', '136.1.1.100')->first();
+if (!$olt) {
+    $olt = App\Models\Olt::where('brand', 'like', '%ZTE%')->first();
+}
+echo 'OLT: ' . $olt->name . ' (id=' . $olt->id . ', ' . $olt->ip_address . ', brand=' . $olt->brand . ')' . PHP_EOL;
 
 $helper = App\Helpers\Olt\OltFactory::make($olt);
 $result = $helper->getUnregisteredOnus();
