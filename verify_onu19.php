ecbae300cc900886dc14a104ded0c7f600aec800
chandra35 <chandra35ok@gmail.com>
2026-04-19 18:34:36 +0700

    tmp: update verify script

--- verify_onu19.php
+++ verify_onu19.php
@@ -1,5 +1,5 @@
 <?php
-// Verify ONU 19 current config after fix
+// Verify ONU 19 current config and status after fix
 
 require __DIR__ . '/vendor/autoload.php';
 $app = require __DIR__ . '/bootstrap/app.php';
@@ -10,5 +10,8 @@ $helper = App\Helpers\Olt\OltFactory::make($olt);
 $ref = new ReflectionMethod($helper, 'executeBatchCliCommands');
 $ref->setAccessible(true);
 
-echo "=== gpon-onu_1/1/1:19 running-config ===\n";
+echo "=== ONU 19 detail-info ===\n";
+echo $ref->invoke($helper, ['show gpon onu detail-info gpon-onu_1/1/1:19']);
+
+echo "\n\n=== ONU 19 gpon-onu running-config ===\n";
 echo $ref->invoke($helper, ['show running-config interface gpon-onu_1/1/1:19']);
