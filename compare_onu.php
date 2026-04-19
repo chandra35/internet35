63ac06fac7d96a0ebe82535ca0d771a9a8907a55
chandra35 <chandra35ok@gmail.com>
2026-04-19 13:14:06 +0700

    tmp: read pon-onu-mng via config mode

--- compare_onu.php
+++ compare_onu.php
@@ -37,17 +37,17 @@ $out .= telnetCmd($sock, "show running-config interface gpon-onu_1/1/1:19", 2);
 $out .= "\n\n=== ONU 20 gpon-onu running-config ===\n";
 $out .= telnetCmd($sock, "show running-config interface gpon-onu_1/1/1:20", 2);
 
-$out .= "\n\n=== ONU 19 pon-onu-mng running-config ===\n";
-$out .= telnetCmd($sock, "show running-config interface pon-onu-mng_1/1/1:19", 3);
+$out .= "\n\n=== ONU 19 pon-onu-mng (via config mode) ===\n";
+$out .= telnetCmd($sock, "configure terminal", 1);
+$out .= telnetCmd($sock, "interface gpon-onu-mng_1/1/1:19", 1);
+$out .= telnetCmd($sock, "show running-config", 3);
+$out .= telnetCmd($sock, "end", 1);
 
-$out .= "\n\n=== ONU 20 pon-onu-mng running-config ===\n";
-$out .= telnetCmd($sock, "show running-config interface pon-onu-mng_1/1/1:20", 3);
-
-$out .= "\n\n=== ONU 19 gpon onu detail ===\n";
-$out .= telnetCmd($sock, "show gpon onu detail-info gpon-onu_1/1/1:19", 3);
-
-$out .= "\n\n=== ONU 20 gpon onu detail ===\n";
-$out .= telnetCmd($sock, "show gpon onu detail-info gpon-onu_1/1/1:20", 3);
+$out .= "\n\n=== ONU 20 pon-onu-mng (via config mode) ===\n";
+$out .= telnetCmd($sock, "configure terminal", 1);
+$out .= telnetCmd($sock, "interface gpon-onu-mng_1/1/1:20", 1);
+$out .= telnetCmd($sock, "show running-config", 3);
+$out .= telnetCmd($sock, "end", 1);
 
 fclose($sock);
 
