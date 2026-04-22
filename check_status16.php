5bb778621b1111b17c138281f9ddd90b24059935
chandra35 <chandra35ok@gmail.com>
2026-04-22 20:59:55 +0700

    tmp

---
+++ check_status16.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+$app = require __DIR__.'/bootstrap/app.php';
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+$o = App\Models\Onu::find(16);
+echo json_encode([
+    'status'      => $o->status,
+    'olt_rx'      => $o->olt_rx_power,
+    'rx'          => $o->rx_power,
+    'tx'          => $o->tx_power,
+    'sn'          => $o->serial_number,
+    'updated_at'  => (string) $o->updated_at,
+    'last_seen'   => (string) $o->last_seen_at,
+    'signal_q'    => $o->signal_quality,
+], JSON_PRETTY_PRINT) . PHP_EOL;
