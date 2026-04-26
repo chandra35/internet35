6cce0e5082e5ae405fe4e8689c38e281575d8294
chandra35 <chandra35ok@gmail.com>
2026-04-26 17:17:29 +0700

    fix: rename duplicate route name tr069-security POST -> tr069-security-set

--- routes/web.php
+++ routes/web.php
@@ -439,7 +439,7 @@ Route::prefix('admin')->middleware(['auth', 'role:superadmin|admin|admin-pop|tek
         Route::post('/{onu}/tr069-firmware', [\App\Http\Controllers\Admin\OnuController::class, 'downloadFirmwareTr069'])->name('tr069-firmware');
         Route::post('/{onu}/tr069-inform-interval', [\App\Http\Controllers\Admin\OnuController::class, 'setTr069InformInterval'])->name('tr069-inform-interval');        Route::post('/{onu}/tr069-provisioning-code', [\App\Http\Controllers\Admin\OnuController::class, 'setTr069ProvisioningCode'])->name('tr069-provisioning-code');
         Route::post('/{onu}/tr069-user-password', [\App\Http\Controllers\Admin\OnuController::class, 'changeTr069UserPassword'])->name('tr069-user-password');
-        Route::post('/{onu}/tr069-security', [\App\Http\Controllers\Admin\OnuController::class, 'setTr069Security'])->name('tr069-security');
+        Route::post('/{onu}/tr069-security', [\App\Http\Controllers\Admin\OnuController::class, 'setTr069Security'])->name('tr069-security-set');
         Route::post('/{onu}/tr069-lan', [\App\Http\Controllers\Admin\OnuController::class, 'setTr069Lan'])->name('tr069-lan');
         Route::get('/{onu}/tr069-blocked-clients', [\App\Http\Controllers\Admin\OnuController::class, 'getBlockedClients'])->name('tr069-blocked-clients');
         Route::post('/{onu}/tr069-block-client', [\App\Http\Controllers\Admin\OnuController::class, 'blockTr069Client'])->name('tr069-block-client');
